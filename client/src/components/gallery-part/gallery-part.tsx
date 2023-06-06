14d9b3d0cd227c7961bfa8ccf969be4178516ada
Valerii Artemenko <87828092+ValeriiArt@users.noreply.github.com>
2023-06-06 12:47:32 +0300

    fix img gallery

--- client/src/components/gallery-part/gallery-part.tsx
+++ client/src/components/gallery-part/gallery-part.tsx
@@ -9,31 +9,7 @@ import Thumbnails from "yet-another-react-lightbox/plugins/thumbnails";
 import Zoom from "yet-another-react-lightbox/plugins/zoom";
 import "yet-another-react-lightbox/plugins/thumbnails.css";
 
-import img1 from "../../../public/images/gallery/1.jpg";
-import img2 from "../../../public/images/gallery/2.jpg";
-import img3 from "../../../public/images/gallery/3.jpg";
-import img4 from "../../../public/images/gallery/4.jpg";
-import img5 from "../../../public/images/gallery/5.jpg";
-import img6 from "../../../public/images/gallery/6.jpg";
-import img7 from "../../../public/images/gallery/7.jpg";
-import img8 from "../../../public/images/gallery/8.jpg";
-import img9 from "../../../public/images/gallery/9.jpg";
-import img10 from "../../../public/images/gallery/10.jpg";
-import img11 from "../../../public/images/gallery/11.jpg";
 
-export const photos = [
-    { src: img2, width: 1600, height: 900 },
-    { src: img3, width: 800, height: 600 },
-    { src: img10, width: 1600, height: 900 },
-    { src: img7, width: 1600, height: 900 },
-    { src: img6, width: 800, height: 600 },
-    { src: img1, width: 800, height: 600 },
-    { src: img11, width: 1600, height: 900 },
-    { src: img9, width: 1600, height: 900 },
-    { src: img8, width: 1600, height: 900 },
-    { src: img5, width: 1600, height: 900 },
-    { src: img4, width: 800, height: 600 },
-];
 
 // const breakpoints = [1080, 640, 384, 256, 128, 96, 64, 48];
 
@@ -78,15 +54,15 @@ export const photos = [
 //  }),
 // }));
 
-// type Properties = {
-//     photos: {
-//         src: string,
-//         width: number,
-//         height: number,
-//     }[],
-// }
+type Properties = {
+    photos: {
+        src: string,
+        width: number,
+        height: number,
+    }[],
+}
 
-const GalleryPart = () => {
+const GalleryPart: React.FC<Properties> = ({ photos }) => {
     const [index, setIndex] = useState(-1);
 
     return (
--- client/src/components/gallery-part/mockData.ts
+++ client/src/components/gallery-part/mockData.ts
@@ -1,14 +1,14 @@
-import img1 from "../../../public/images/gallery/1.jpg";
-import img2 from "../../../public/images/gallery/2.jpg";
-import img3 from "../../../public/images/gallery/3.jpg";
-import img4 from "../../../public/images/gallery/4.jpg";
-import img5 from "../../../public/images/gallery/5.jpg";
-import img6 from "../../../public/images/gallery/6.jpg";
-import img7 from "../../../public/images/gallery/7.jpg";
-import img8 from "../../../public/images/gallery/8.jpg";
-import img9 from "../../../public/images/gallery/9.jpg";
-import img10 from "../../../public/images/gallery/10.jpg";
-import img11 from "../../../public/images/gallery/11.jpg";
+import img1 from "/public/images/gallery/1.jpg";
+import img2 from "/public/images/gallery/2.jpg";
+import img3 from "/public/images/gallery/3.jpg";
+import img4 from "/public/images/gallery/4.jpg";
+import img5 from "/public/images/gallery/5.jpg";
+import img6 from "/public/images/gallery/6.jpg";
+import img7 from "/public/images/gallery/7.jpg";
+import img8 from "/public/images/gallery/8.jpg";
+import img9 from "/public/images/gallery/9.jpg";
+import img10 from "/public/images/gallery/10.jpg";
+import img11 from "/public/images/gallery/11.jpg";
 
 export const photos = [
     { src: img2, width: 1600, height: 900 },
