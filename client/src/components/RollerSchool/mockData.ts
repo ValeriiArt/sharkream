98d9d3788584e8134d868a0528a2e25e15a4bc5a
Valerii Artemenko <87828092+ValeriiArt@users.noreply.github.com>
2023-06-06 15:31:43 +0300

    fix img roller-school

--- client/src/components/RollerSchool/mockData.ts
+++ client/src/components/RollerSchool/mockData.ts
@@ -1,3 +1,7 @@
+import locationImg1 from "/public/images/roller-school/roller-4.webp"
+import locationImg2 from "/public/images/roller-school/roller-3.webp"
+import locationImg3 from "/public/images/roller-school/roller-5.webp"
+
 type LocationsData = {
  name: string;
     srcImage: string;
@@ -7,17 +11,17 @@ type LocationsData = {
 export const locations: LocationsData = [
     {
         name: "Паркове Місто",
-        srcImage: "../public/images/roller-school/roller-4.webp",
+        srcImage: locationImg1,
         srcLocation: 'https://goo.gl/maps/QSf1foye2cvEFmLW7?coh=178571&entry=tt'
     },
     {
         name: "ТЦ Променада",
-        srcImage: "../public/images/roller-school/roller-3.webp",
+        srcImage: locationImg2,
        srcLocation: 'https://goo.gl/maps/kHuyPc9pXbD1dCJGA?coh=178571&entry=tt'
     },
     {
         name: "Парк Наталка",
-        srcImage: "../public/images/roller-school/roller-5.webp",
+        srcImage: locationImg3,
         srcLocation: 'https://goo.gl/maps/jTr1UFR1DT4rJ5uF8?coh=178571&entry=tt'
     },
 ];
