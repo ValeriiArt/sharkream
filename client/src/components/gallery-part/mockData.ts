319a030290c77a588a21ebdd9c294e9e73e00438
Valerii Artemenko <87828092+ValeriiArt@users.noreply.github.com>
2023-06-06 13:37:20 +0300

    fix img gallery

--- client/src/components/gallery-part/mockData.ts
+++ client/src/components/gallery-part/mockData.ts
@@ -10,16 +10,16 @@
 // import img10 from "/public/images/gallery/10.jpg";
 // import img11 from "/public/images/gallery/11.jpg";
 
-// export const photos = [
-//     { src: img2, width: 1600, height: 900 },
-//     { src: img3, width: 800, height: 600 },
-//     { src: img10, width: 1600, height: 900 },
-//     { src: img7, width: 1600, height: 900 },
-//     { src: img6, width: 800, height: 600 },
-//     { src: img1, width: 800, height: 600 },
-//     { src: img11, width: 1600, height: 900 },
-//     { src: img9, width: 1600, height: 900 },
-//     { src: img8, width: 1600, height: 900 },
-//     { src: img5, width: 1600, height: 900 },
-//     { src: img4, width: 800, height: 600 },
-// ];
+export const photos = [
+    // { src: img2, width: 1600, height: 900 },
+    // { src: img3, width: 800, height: 600 },
+    // { src: img10, width: 1600, height: 900 },
+    // { src: img7, width: 1600, height: 900 },
+    // { src: img6, width: 800, height: 600 },
+    // { src: img1, width: 800, height: 600 },
+    // { src: img11, width: 1600, height: 900 },
+    // { src: img9, width: 1600, height: 900 },
+    // { src: img8, width: 1600, height: 900 },
+    // { src: img5, width: 1600, height: 900 },
+    // { src: img4, width: 800, height: 600 },
+];
