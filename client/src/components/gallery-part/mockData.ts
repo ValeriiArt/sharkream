24df33224160eb8ce21c6a264b004e3d5b050b40
Valerii Artemenko <87828092+ValeriiArt@users.noreply.github.com>
2023-06-07 12:22:13 +0300

    fix gallery

--- client/src/components/gallery-part/mockData.ts
+++ client/src/components/gallery-part/mockData.ts
@@ -1,12 +1,12 @@
 
 
 export const photos = [
-    { src: "/public/images/gallery/2.jpg", width: 1600, height: 900 },
-    { src: "/public/images/gallery/3.jpg", width: 800, height: 600 },
-    { src: "/public/images/gallery/10.jpg", width: 1600, height: 900 },
-    { src: "/public/images/gallery/7.jpg", width: 1600, height: 900 },
-    { src: "/public/images/gallery/6.jpg", width: 800, height: 600 },
-    { src: "/public/images/gallery/1.jpg", width: 800, height: 600 },
+    { src: "/images/gallery/2.jpg", width: 1600, height: 900 },
+    { src: "/images/gallery/3.jpg", width: 800, height: 600 },
+    { src: "/images/gallery/10.jpg", width: 1600, height: 900 },
+    { src: "/images/gallery/7.jpg", width: 1600, height: 900 },
+    { src: "/images/gallery/6.jpg", width: 800, height: 600 },
+    { src: "/images/gallery/1.jpg", width: 800, height: 600 },
     { src: "/public/images/gallery/11.jpg", width: 1600, height: 900 },
     { src: "/public/images/gallery/9.jpg", width: 1600, height: 900 },
     { src: "/public/images/gallery/8.jpg", width: 1600, height: 900 },
