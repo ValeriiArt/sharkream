475bf7d44942704849242a7e051997351df39387
Valerii Artemenko <87828092+ValeriiArt@users.noreply.github.com>
2023-06-10 13:00:24 +0300

    fix cors

--- server/config/plagins.js
+++ server/config/plagins.js
@@ -1,5 +1,19 @@
 module.exports = ({ env }) => ({
- // ...
+ "cloudflare-pages": {
+  enabled: true,
+  config: {
+   instances: [
+    {
+     name: "production website",
+     hook_url: "*",
+    },
+    {
+     name: "preview website",
+     hook_url: "*",
+    },
+   ],
+  },
+ },
  seo: {
   enabled: true,
  },
